ae648087d639601bc5fab5176a701bd57bc6a790
Kevin Tang <kevintang2023@gmail.com>
2020-12-20 19:40:48 -0800

    TCP version

--- files/lab4c_tcp.c
+++ files/lab4c_tcp.c
@@ -2,15 +2,17 @@
 // EMAIL: kevintang2023@ucla.edu
 // ID: 805419480
 
-#include <getopt.h>
-#include <math.h>
-
+#include <arpa/inet.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <getopt.h>
+#include <math.h>
+#include <netdb.h>
 #include <poll.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/socket.h>
 #include <sys/stat.h>
 #include <sys/time.h>
 #include <sys/types.h>
@@ -21,36 +23,53 @@
 #include <mraa.h>
 #include <mraa/aio.h>
 mraa_aio_context grove_sensor;
-mraa_gpio_context grove_button;
 #endif
 
 // argument options
 int opt_period = 1;
 int opt_scale  = 0;  // 0 = F, 1 = C
 int opt_log    = 0;
-FILE *opt_file   = 0;
-int opt_debug  = 0;
 
+FILE* opt_file = 0;
+char* opt_host = NULL;
+char* opt_id   = NULL;
+int opt_debug  = 0;
+int opt_port = -1;
 // stdin Options
 int opt_report = 1;
 
 #define INPUT_SIZE 1023
 
 // Some help from https://www.tutorialspoint.com/c_standard_library/c_function_localtime.htm
-struct tm * time_info;
+struct tm* time_info;
 struct timeval time_now;
 time_t last_report;
 time_t current;
+
+// Socket vars
+int sock_fd = 0;
+int rc      = 0;
+struct sockaddr_in server_address;
+struct hostent* host;
 
 void log_and_print(char* string) {
     if (opt_log != 0) {
         fprintf(opt_file, "%s\n", string);
         fflush(opt_file);
     }
-    fprintf(stdout, "%s\n", string);
+    dprintf(sock_fd, "%s\n", string);
+    fprintf(stderr, "%s\n", string);
 }
 
-void button_shutdown() {
+void error_msg(int returnValue, char* message) {
+    if (returnValue < 0) {
+        fprintf(stderr, "Error %s. errno %d: %s\r\n", message, errno, strerror(errno));
+        exit(errno);
+    }
+    return;
+}
+
+void shut_down() {
     char temp[128];
     time_info = localtime(&time_now.tv_sec);
     sprintf(temp, "%02d:%02d:%02d SHUTDOWN", time_info->tm_hour, time_info->tm_min, time_info->tm_sec);
@@ -58,29 +77,27 @@ void button_shutdown() {
     if (opt_log)
         fclose(opt_file);
 
-    #ifndef PC
+#ifndef PC
     mraa_aio_close(grove_sensor);
-    mraa_gpio_close(grove_button);
-    #endif
+#endif
     exit(0);
 }
 
 double parse_reading(int reading) {
     // https://wiki.seeedstudio.com/Grove-Temperature_Sensor_V1.2/
-    const double B = 4275.;
-    const double R0 = 100000.;
-    double t = 1023.0 / (double) reading - 1.0;
-    t = R0 * t;
-    double log_result = log(t / R0);
-    double temperature = 1.0/(log_result/B+1/298.15)-273.15; // Convert to temperature via datasheet
-    
-    if (opt_scale == 0) // F
+    const double B     = 4275.;
+    const double R0    = 100000.;
+    double t           = 1023.0 / (double)reading - 1.0;
+    t                  = R0 * t;
+    double log_result  = log(t / R0);
+    double temperature = 1.0 / (log_result / B + 1 / 298.15) - 273.15;  // Convert to temperature via datasheet
+
+    if (opt_scale == 0)  // F
     {
         return (temperature * 9. / 5.) + 32;
     } else {
         return temperature;
     }
-    
 }
 
 // option_string is a \0 terminated string w/ one of the options
@@ -98,11 +115,10 @@ void parse_option(char* option_string) {
     } else if (strcmp(option_string, "START") == 0) {
         opt_report = 1;
     } else if (strcmp(option_string, "OFF") == 0) {
-        button_shutdown();
+        shut_down();
     } else {
         char* found_period = strstr(option_string, "PERIOD=");
-        if (found_period != NULL)
-        {
+        if (found_period != NULL) {
             // move found_period to beginning of arg
             // technically not needed for atoi(), but why not
             found_period += 7;
@@ -112,8 +128,7 @@ void parse_option(char* option_string) {
         // We don't technically need to look for LOG <message> since all non-valid commands are automatically outputted anyways
         // We just find it and do nothing
         char* found_log = strstr(option_string, "LOG ");
-        if (found_log != NULL)
-        {
+        if (found_log != NULL) {
             // move found_log to beginning of arg
             found_period += 4;
         }
@@ -127,15 +142,16 @@ int main(int argc, char** argv) {
             {"period", required_argument, NULL, 1},
             {"scale", required_argument, NULL, 2},
             {"log", required_argument, NULL, 3},
-            {"debug", no_argument, NULL, 4},
+            {"host", required_argument, NULL, 4},
+            {"id", required_argument, NULL, 5},
             {0, 0, 0, 0}};
 
     for (;;) {
         int opt_index = 0;
-        int c        = getopt_long(argc, argv, "", options, &opt_index);
+        int c         = getopt_long(argc, argv, "", options, &opt_index);
 
         if (c == -1)
-            break;  
+            break;
 
         switch (c) {
             case 1:
@@ -147,7 +163,7 @@ int main(int argc, char** argv) {
                 } else if (strcmp(optarg, "C") == 0) {
                     opt_scale = 1;
                 } else {
-                    fprintf(stderr, "Invalid scale argument, try \"lab4b [--period=<seconds>] [--log=<log_filename>] [--scale=F/C] [--debug]\"\n\n");
+                    fprintf(stderr, "Invalid scale argument, please try again\n\n");
                     exit(1);
                 }
                 break;
@@ -159,36 +175,85 @@ int main(int argc, char** argv) {
                     exit(1);
                 }
                 break;
-
             case 4:
-                opt_debug = 1;
+                opt_host = optarg;
+                if (strlen(opt_host) == 0) {
+                    fprintf(stderr, "Invalid hostname argument, please try again\n");
+                    exit(1);
+                }
+                break;
+            case 5:
+                opt_id = optarg;
+                if (strlen(opt_id) != 9) {
+                    fprintf(stderr, "Invalid ID argument, please try again\n");
+                    exit(1);
+                }
                 break;
 
             default:
-                fprintf(stderr, "Try \"lab4b [--period=<seconds>] [--log=<log_filename>] [--scale=F/C] [--debug]\"\n\n");
+                fprintf(stderr, "Try \"lab4c [--period=<seconds>] [--log=<log_filename>] [--scale=F/C] [--id=ID] [--host=HOSTNAME] [--debug] port-number\"\n\n");
                 exit(1);
                 break;
         }
     }
 
-    #ifndef PC
+    if (optind < argc) {
+        opt_port = atoi(argv[optind]);
+        if (opt_port < 0 || opt_port > 65535) {
+            fprintf(stderr, "Invalid port number, please try again");
+            exit(1);
+        }
+    } else {
+        fprintf(stderr, "Missing port number. Try \"lab4c [--period=<seconds>] [--log=<log_filename>] [--scale=F/C] [--id=ID] [--host=HOSTNAME] [--debug] port-number\"\n\n");
+        exit(1);
+    }
+
+    if (opt_log == 0 || opt_host == NULL || opt_id == NULL) {
+        // Some help from:
+        // https://www.gnu.org/software/libc/manual/html_node/Using-Getopt.html
+        fprintf(stderr, "Missing required options. Try \"lab4c [--period=<seconds>] [--log=<log_filename>] [--scale=F/C] [--id=ID] [--host=HOSTNAME] [--debug] port-number\"\n\n");
+        exit(1);
+    }
+
+#ifndef PC
     grove_sensor = mraa_aio_init(1);
-    grove_button = mraa_gpio_init(60);
-    
-    if (grove_sensor == NULL || grove_button == NULL)
+    if (grove_sensor == NULL)  // || grove_button == NULL)
     {
         fprintf(stderr, "Error: failed to initialize sensors. errno %d: %s\r\n", errno, strerror(errno));
         exit(1);
     }
-    
-    mraa_gpio_dir(grove_button, MRAA_GPIO_IN);
-    mraa_gpio_isr(grove_button, MRAA_GPIO_EDGE_RISING, &button_shutdown, NULL);
-    #endif
+#endif
+
+    // Create a socket
+
+    sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+    error_msg(sock_fd, "while creating socket");
+
+    bzero((char*)&server_address, sizeof(server_address));
+    server_address.sin_family = AF_INET;
+
+    host = gethostbyname(opt_host);
+    if (host == NULL) {
+        error_msg(-1, "while finding host");
+    }
+
+    bcopy((char*)host->h_addr, (char*)&server_address.sin_addr.s_addr, host->h_length);
+
+    server_address.sin_port = htons(opt_port);
+
+
+    int address_length = sizeof(server_address);
+
+    // Connect to server using - connect()
+    rc = connect(sock_fd, (struct sockaddr*)&server_address, address_length);
+    error_msg(rc, "while connecting to server");
+
+    dprintf(sock_fd, "ID=%s\n", opt_id);
 
     char buffer[INPUT_SIZE + 1];
 
     struct pollfd poll_fd;
-    poll_fd.fd     = STDIN_FILENO;
+    poll_fd.fd     = sock_fd;
     poll_fd.events = POLLIN | POLLERR;
 
     int poll_rc = 0;
@@ -201,13 +266,13 @@ int main(int argc, char** argv) {
         time(&current);
         gettimeofday(&time_now, 0);
 
-        if (opt_report == 1 && difftime(current, last_report) >= (double) opt_period) {
+        if (opt_report == 1 && difftime(current, last_report) >= (double)opt_period) {
             time_info = localtime(&time_now.tv_sec);
-            #ifndef PC
+#ifndef PC
             int sensor_reading = mraa_aio_read(grove_sensor);
-            #else
+#else
             int sensor_reading = 100;
-            #endif
+#endif
             float temperature = parse_reading(sensor_reading);
             char temp[128];
             sprintf(temp, "%02d:%02d:%02d %.1f", time_info->tm_hour, time_info->tm_min, time_info->tm_sec, temperature);
@@ -227,40 +292,35 @@ int main(int argc, char** argv) {
                 fprintf(stderr, "Error reading. errno %d: %s\r\n", errno, strerror(errno));
                 exit(1);
             }
-            
+
             // lex input
             buffer[read_rc] = '\0';
 
             char* head = &buffer[0];
 
             // Ignore whitespace
-            while(*head != '\0') {
-                while(*head == ' ' || *head == '\t')
+            while (*head != '\0') {
+                while (*head == ' ' || *head == '\t')
                     head++;
 
                 char* tail = strstr(head, "\n");
-                
-                int size = tail - head;
-                char* arg_buffer = (char*) malloc((size + 1) * sizeof(char));
+
+                int size         = tail - head;
+                char* arg_buffer = (char*)malloc((size + 1) * sizeof(char));
                 strncpy(arg_buffer, head, size);
                 arg_buffer[size] = '\0';
                 parse_option(arg_buffer);
                 free(arg_buffer);
                 head = tail + 1;
             }
-
-            
-
         }
-
     }
 
     if (opt_log) {
         fclose(opt_file);
     }
 
-    #ifndef PC
+#ifndef PC
     mraa_aio_close(grove_sensor);
-    mraa_gpio_close(grove_button);
-    #endif
+#endif
 }
